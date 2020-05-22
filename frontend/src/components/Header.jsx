0c0d5c61f65049367fdaa87c175c7f596da07112
BereSoto <beresoto.f@gmail.com>
2020-05-22 02:52:57 -0500

    creating footer

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,12 +7,12 @@ const Header = () => (
     <div className='header__menu'>
       <ul className='header__menu-items'>
         <li className='header__menu-items--item'>
-          <a href='#'>Acerca de</a>
+          <a href='#'>Acerca de </a>
           {' '}
         </li>
         <li className='header__menu-items--item loguin'>
           {' '}
-          <a href='#'>Iniciar sesión</a>
+          <a href='#'>Iniciar sesión </a>
         </li>
       </ul>
     </div>
