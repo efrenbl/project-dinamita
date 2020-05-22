43d388a2c2e49d983e43fa73b49f8286fa75d2f0
BereSoto <beresoto.f@gmail.com>
2020-05-22 01:59:18 -0500

    header styles and structure

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 const Header = () => (
 
   <header className='header'>
-    <div className='header_menu'>
-      <p>
-        Hola soy un header de prueba &#128147;
-
-      </p>
+    <p>logo</p>
+    <div className='header__menu'>
+      <ul className='header__menu-items'>
+        <li className='header__menu-items--item'> Acerca de </li>
+        <li className='header__menu-items--item loguin'>Iniciar sesión</li>
+      </ul>
     </div>
   </header>
 
