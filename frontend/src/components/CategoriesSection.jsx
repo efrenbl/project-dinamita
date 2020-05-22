cad6f363ec66098c129be92241394fb4725865dc
BereSoto <beresoto.f@gmail.com>
2020-05-22 03:26:25 -0500

    create categories section

---
+++ frontend/src/components/CategoriesSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CategoriesSection = () => (
+  <section className='seccion1' />
+
+);
+
+export default CategoriesSection;
+
--- frontend/src/containers/Home.jsx
+++ frontend/src/containers/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import CategoriesSection from '../components/CategoriesSection';
 
 const Home = () => (
   <div className='Home'>
     <Header />
+    <CategoriesSection />
     <Footer />
 
   </div>
