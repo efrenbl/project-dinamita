a59a8e3da751dd9679b52378c34030def13deedc
BereSoto <beresoto.f@gmail.com>
2020-05-22 03:14:11 -0500

    styles footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer>
-    <p>soy un footer</p>
+  <footer className='footer'>
+    <ul className='footer__items'>
+      <li><a href='#' className='footer__items-item'>Aviso de privacidad</a></li>
+      <li><a href='#' className='footer__items-item'> Proyecto Notibot 2020 ©</a></li>
+    </ul>
   </footer>
 
 );
