0946c267e2197b653ab5920bdc687143ab155949
BereSoto <beresoto.f@gmail.com>
2020-05-22 04:16:46 -0500

    title of politics

--- frontend/src/components/CategoriesSection.jsx
+++ frontend/src/components/CategoriesSection.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import arrow from '../assets/static/arrow.svg';
+import triangulos from '../assets/static/triangulos.svg';
 
 const CategoriesSection = () => (
-  <section className='seccion1' />
+  <section className='political'>
+    <div className='political__section'>
+      <div className='political__section-title'>
+        <p>Politica</p>
+        <img className='arrow' src={arrow} alt='arrow' />
+      </div>
+      <div className='political__section-patron'>
+        <img className='arrow' src={triangulos} alt='arrow' />
+
+      </div>
+
+    </div>
+  </section>
 
 );
 
--- frontend/src/containers/Home.jsx
+++ frontend/src/containers/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+
 import CategoriesSection from '../components/CategoriesSection';
 
 const Home = () => (
