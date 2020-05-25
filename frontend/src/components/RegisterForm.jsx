dedd5afc517f1552fa4f4459498bf021861a72b6
SilviaAlarcon <silvia.alarcon.or@gmail.com>
2020-05-25 06:33:12 -0500

    registration form added

---
+++ frontend/src/components/RegisterForm.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const RegisterForm = () => (
+  <section className='register'>
+    <section className='register__container'>
+      <h2>Crear cuenta</h2>
+      <form className='register__container--form'>
+        <input className='input' type='text' placeholder='Correo' />
+        <input className='input' type='password' placeholder='Contraseña' />
+        <input className='input' type='password' placeholder='Confirma la contraseña' />
+        <button className='button' type='button'> Registrarse </button>
+      </form>
+      <div className='register__container--login'>
+        <a href=''> Inicia sesión </a>
+      </div>
+    </section>
+  </section>
+);
+
+export default RegisterForm;
---
+++ frontend/src/containers/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/Header';
+import RegisterForm from '../components/RegisterForm';
+
+const Register = () => (
+  <div className='Register'>
+    <Header />
+    <RegisterForm />
+  </div>
+);
+
+export default Register;
