f2f65b9a6ae232ac508c2c2e4285c9e53245e079
Efren Bautista <ing.efrenbl@gmail.com>
2020-05-27 21:50:48 -0500

    chanching database setup

--- backend/src/notibot/settings.py
+++ backend/src/notibot/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'notibot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'oligopolio',
+        'HOST': 'localhost', 
+        'PORT': '5433',
     }
 }
+
 
 
 # Password validation
