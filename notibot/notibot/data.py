1aa3f9e9b18801718439b2bd818603ade7de35bc
BereSoto <beresoto.f@gmail.com>
2020-05-29 04:14:40 -0500

    creating the base to clean the data

--- notibot/notibot/data.py
+++
@@ -1,8 +0,0 @@
-
-import pandas as pd
-
-
-el_universal = pd.read_csv('eluniversal_2020_05_28_articles.csv',sep="|")
-
-el_universal.head()
-
---
+++ notibot/notibot/newspaper.py
@@ -0,0 +1,61 @@
+import argparse
+import logging
+logging.basicConfig(level=logging.INFO)
+from urllib.parse import urlparse
+
+import pandas as pd 
+
+logger = logging.getLogger(__name__) #referencia a nuestro logger
+
+
+def main(filename):
+    logger.info('Init cleaning process')
+
+    df = _read_data(filename)
+    newspaper_uid = _extract_newspaper_uid(filename)
+    df = _add_newspaper_uid_column(df, newspaper_uid) #regresa el dataframe modificado
+    df = _extract_host(df) #extraemos el host
+
+    return df
+
+
+def _read_data(filename): #declaramos funcion read_data
+    logger.info('reading file {}'.format(filename))
+
+    return pd.read_csv(filename)
+    
+    #pd.read_csv(filename,encoding='cp1252')
+    #return pd.read_csv(filename, encoding = "latin-1")
+
+
+def _extract_newspaper_uid(filename):
+    logger.info('Extract newspaper uid')
+    newspaper_uid = filename.split('_')[0]
+    logger.info('Detected: {}'.format(newspaper_uid))
+    return newspaper_uid
+
+
+def _add_newspaper_uid_column(df,newspaper_uid):
+    logger.info('Filling newspaper_uid column with {}'.format(newspaper_uid))
+    df['newspaper_uid'] = newspaper_uid  #agregamos una nueva columna newspaper_uid, sirve para cualquier periodico
+
+    return df
+
+def _extract_host(df):
+    logger.info('Host froms urls')
+    df['host']=df['url'].applay(lambda url: urlparse(url).netloc)
+
+    return df
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser() #cual va a ser el dataset que se trabajara
+    parser.add_argument('filename',
+                        help='Dirty data',  #camino o ruta a los datos sucios
+                        type=str )
+
+
+args = parser.parse_args()
+
+df = main(args.filename) 
+
+print(df)
