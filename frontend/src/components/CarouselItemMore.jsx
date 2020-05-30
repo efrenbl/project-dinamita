be4561fa872e690655f6565efb738b33c02da83a
SilviaAlarcon <silvia.alarcon.or@gmail.com>
2020-05-30 18:25:46 -0500

    test

---
+++ frontend/src/components/CarouselItemMore.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import more from '../assets/static/more.svg';
+
+const CarouselItemMore = () => (
+  <div className='carousel-item'>
+    <img className='carousel-item__img' src={more} alt='' />
+  </div>
+);
+
+export default CarouselItemMore;
