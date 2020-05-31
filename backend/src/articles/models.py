30ebba3624c5440c21f5c3b7612ff99dc32ce340
Efren Bautista <ing.efrenbl@gmail.com>
2020-05-31 00:00:09 -0500

    add users model

--- backend/src/articles/models.py
+++ backend/src/articles/models.py
@@ -3,10 +3,17 @@ from __future__ import unicode_literals
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.auth.models import User
 
 from django.db import models
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, default = 1)
+    
+    def __str__(self):
+        return self.author.username
 
 class Article(models.Model):
     #content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, default= 1)
