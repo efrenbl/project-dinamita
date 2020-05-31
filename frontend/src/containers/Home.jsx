6468a30116936248a8980ec4711d6260e6c2ce67
SilviaAlarcon <silvia.alarcon.or@gmail.com>
2020-05-31 05:42:31 -0500

    hooks

--- frontend/src/containers/Home.jsx
+++ frontend/src/containers/Home.jsx
@@ -2,58 +2,59 @@ import React, { useState, useEffect } from 'react';
 import Categories from '../components/Categories';
 import CarouselItem from '../components/CarouselItem';
 import CarouselItemMore from '../components/CarouselItemMore';
+import useInitialState from '../hooks/useInitialState';
+
+const APIInternacional = 'http://127.0.0.1:8000/api/articles/?q=internacional';
+const APINacional = 'http://127.0.0.1:8000/api/articles/?q=nacional';
+const APIFinanzas = 'http://127.0.0.1:8000/api/articles/?q=finanzas';
+const APIDeportes = 'http://127.0.0.1:8000/api/articles/?q=deportes';
 
 const Home = () => {
+  const initialStateInternacional = useInitialState(APIInternacional);
+  const initialStateNacional = useInitialState(APINacional);
+  const initialStateFinanzas = useInitialState(APIFinanzas);
+  const initialStateDeportes = useInitialState(APIDeportes);
 
-  const [articles, setArticles] = useState([]);
-
-  useEffect(() => {
-    fetch('http://127.0.0.1:8000/api/articles/')
-      .then(response => response.json())
-      .then(data => setArticles(data));
-  }, []);
-
-  console.log(articles);
   return (
     <div className='Home'>
 
-      {articles.map(item =>
+      {initialStateInternacional.map(item =>
         <Categories key={item.category} {...item} >
-          {articles.map(item =>
+          {initialStateInternacional.map(item =>
             <CarouselItem key={item.id} {...item} />
           )}
           <CarouselItemMore />
         </Categories>
       )}
 
-      {/*       <Categories title='Nacional'>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItemMore />
-      </Categories>
+      {initialStateNacional.map(item =>
+        <Categories key={item.category} {...item} >
+          {initialStateNacional.map(item =>
+            <CarouselItem key={item.id} {...item} />
+          )}
+          <CarouselItemMore />
+        </Categories>
+      )}
 
-      <Categories title='Finanzas'>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItemMore />
-      </Categories>
+      {initialStateFinanzas.map(item =>
+        <Categories key={item.category} {...item} >
+          {initialStateFinanzas.map(item =>
+            <CarouselItem key={item.id} {...item} />
+          )}
+          <CarouselItemMore />
+        </Categories>
+      )}
 
-      <Categories title='Deportes'>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItemMore />
-      </Categories>
+      {initialStateDeportes.map(item =>
+        <Categories key={item.category} {...item} >
+          {initialStateDeportes.map(item =>
+            <CarouselItem key={item.id} {...item} />
+          )}
+          <CarouselItemMore />
+        </Categories>
+      )}
 
-      <Categories title='Espectáculos'>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItemMore />
-      </Categories> */}
+
     </div>
   );
 };
