715b99c03acf5f809cb04942321d924ca935e6f2
Efren Bautista <37494255+efrenbl@users.noreply.github.com>
2020-05-31 19:32:29 -0500

    updating setting 4

--- backend/src/notibot/settings.py
+++ backend/src/notibot/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'notibot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'oligopolio',
