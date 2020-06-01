beebbe7e3a097c98e089b11b8bf17fecb551c433
BereSoto <beresoto.f@gmail.com>
2020-05-31 19:11:40 -0500

    Arreglando overflow de carousel

--- frontend/src/components/Categories.jsx
+++ frontend/src/components/Categories.jsx
@@ -2,19 +2,9 @@ import React from 'react';
 
 const Categories = ({ children, category }) => (
   <section className='categories'>
+    <h2 className='categories__name'>{category}</h2>
     <div className='carousel'>
-      <div className='categories__container'>
-        <h2 className='categories__name'>
-          {category}
-        </h2>
-      </div>
-      <div className='carousel__container'>
-        <div className='carousel'>
-          <div className='carousel__container'>
-            {children}
-          </div>
-        </div>
-      </div>
+      <div className='carousel__container'>{children}</div>
     </div>
   </section>
 );
