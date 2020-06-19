9ebb9bfe8156c21230c84205b49545424fb0fc85
BereSoto <beresoto.f@gmail.com>
2020-06-18 19:13:51 -0500

    eliminado carpetas de frontend

--- frontend/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './assets/styles/main.scss';
-import Home from './containers/Home';
-
-ReactDOM.render(<Home />, document.getElementById('app'));
